9e0aea4b940e38d5d241b594b8ff5881d167c94f
Alex <alex_edisson@hotmail.com>
2021-01-16 14:41:52 -0500

    Adding pagination

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Image } from "../components"
+import { Container, Image, Content, Card } from "../components"
 
 import SEO from "../components/seo"
 
@@ -8,6 +8,14 @@ const IndexPage = () => (
     <SEO title="Home" />
     <Container>
       <Image />
+      <Content>
+        <Card
+          date="Jan 2021"
+          title="First post"
+          excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
+          slug="/text"
+        ></Card>
+      </Content>
     </Container>
   </>
 )
